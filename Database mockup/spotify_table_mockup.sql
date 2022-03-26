5490bf513396c918d0696857ecde6132516ec699
dyl-lee <dylanlee1701@gmail.com>
2022-03-26 01:03:37 -0400

    updated database schema

--- Database mockup/spotify_table_mockup.sql
+++ Database mockup/spotify_table_mockup.sql
@@ -1,20 +1,27 @@
--- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
--- Edited PK/FK constraints
-
-CREATE TABLE track_metadata (
-    track_id varchar   NOT NULL,
-    track_name varchar   NOT NULL,
-    artist_name varchar   NOT NULL,
-    genre varchar   NOT NULL,
-    PRIMARY KEY (track_id)
+CREATE TABLE artist_data (
+    artist_id varchar   NOT NULL,
+    artist_name varchar NOT NULL,
+    followers int   NOT NULL,
+    PRIMARY KEY (artist_id)
 );
 
+CREATE TABLE album_data (
+    album_name varchar   NOT NULL,
+    album_id varchar   NOT NULL,
+    release_date date   NOT NULL,
+    PRIMARY KEY (album_id)
+)
+
 CREATE TABLE track_features (
+    track_name varchar   NOT NULL,
     track_id varchar   NOT NULL,
+    artist_id varchar   NOT NULL,
+    album_id varchar   NOT NULL,
     acousticness float   NOT NULL,
     danceability float   NOT NULL,
     duration_ms int   NOT NULL,
     energy float   NOT NULL,
+    genre varchar   NOT NULL,
     instrumentalness float   NOT NULL,
     key int   NOT NULL,
     liveness float   NOT NULL,
@@ -25,7 +32,8 @@ CREATE TABLE track_features (
     tempo float   NOT NULL,
     time_signature integer   NOT NULL,
     valence float   NOT NULL,
-    FOREIGN KEY (track_id) REFERENCES track_metadata (track_id)
+    FOREIGN KEY (album_id) REFERENCES album_data (album_id)
+    FOREIGN KEY (artist_id) REFERENCES artist_data (artist_id)
 );
 
 
